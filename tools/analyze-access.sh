35ac38087f7811b65c1c6dac8b46271338aa0fd1
nakamuraitsuki <ituki1911n@gmail.com>
2025-11-27 00:03:00 +0900

    fix

--- tools/analyze-access.sh
+++ tools/analyze-access.sh
@@ -6,8 +6,8 @@ PATHS=(
   "/users/transactions.json"
   "/users/[0-9]+.json"
   "/items/*"
-  "/transactions/*.png"
-  "/upload/*.jpg"
+  "/transactions/[0-9]+.png"
+  "/upload/[0-9]+.jpg"
 )
 
 # 出力先ディレクトリを作成
