4f0a67b792e44370a72b43ebc253173f15002b35
nakamuraitsuki <ituki1911n@gmail.com>
2025-11-27 13:02:03 +0900

    fix

--- tools/start-pprof-routein.sh
+++ tools/start-pprof-routein.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go tool pprof -seconds 80 -http=localhost:1080 http://localhost:6060/debug/pprof/goroutine?
+go tool pprof -seconds 80 -http=localhost:1080 http://localhost:6060/debug/pprof/goroutine
