1aa5e7fa8ae1e88c17a8ad3ac118a25089a830c4
nakamuraitsuki <ituki1911n@gmail.com>
2025-11-27 12:44:48 +0900

    計測を長くしてみる

--- tools/start-pprof.sh
+++ tools/start-pprof.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go tool pprof -seconds 60 -http=localhost:1080 http://localhost:6060/debug/pprof/profile
+go tool pprof -seconds 180 -http=localhost:1080 http://localhost:6060/debug/pprof/profile
