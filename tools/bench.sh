1a1e76ab0b997ec29f6b6326d75e2da748eaac1f
nakamuraitsuki <ituki1911n@gmail.com>
2025-11-27 12:28:18 +0900

    add bench script

---
+++ tools/bench.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/isucon/isucari/bin/benchmarker -target-url https://localhost -target-host isucari.t.isucon.pw -data-dir /home/isucon/isucari/initial-data/ -static-dir /home/isucon/isucari/webapp/public/static/ -payment-url https://bp.t.isucon.pw -shipment-url https://bs.t.isucon.pw
