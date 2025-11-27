cccc3a45990d25744b3b3ffc5cb2b32bacc590d1
nakamuraitsuki <ituki1911n@gmail.com>
2025-11-27 12:06:17 +0900

    pprof導入

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"time"
 
@@ -22,7 +23,8 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/crypto/bcrypt"
-	"golang.org/x/sync/errgroup"
+
+	_ "net/http/pprof"
 )
 
 const (
@@ -375,6 +377,12 @@ func main() {
 	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
 		http.FileServerFS(root.FS()).ServeHTTP(w, r)
 	})
+
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
+	go func() {
+		log.Fatal(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
@@ -1040,7 +1048,6 @@ LIMIT ?;
 	}
 
 	itemDetails := make([]ItemDetail, len(items))
-	g := new(errgroup.Group)
 
 	for i, item := range items {
 		idx := i
@@ -1074,29 +1081,19 @@ LIMIT ?;
 			itemDetails[idx].TransactionEvidenceStatus = *it.TransactionEvidenceStatus
 		}
 
-		// TransactionEvidence がある場合だけ goroutine で Shipping API を呼ぶ
-		if itemDetails[idx].TransactionEvidenceID > 0 && it.ReserveID != nil && *it.ReserveID != "" {
-			idx := idx // クロージャに安全に渡す
-			reserveID := it.ReserveID
-			g.Go(func() error {
-				ssr, err := APIShipmentStatus(getShipmentServiceURL(), &APIShipmentStatusReq{
-					ReserveID: *reserveID,
-				})
-				if err != nil {
-					return err
-				}
-				itemDetails[idx].ShippingStatus = ssr.Status
-				return nil
+		// goroutine を使わずに直列実行
+		if itemDetails[i].TransactionEvidenceID > 0 && it.ReserveID != nil && *it.ReserveID != "" {
+			ssr, err := APIShipmentStatus(getShipmentServiceURL(), &APIShipmentStatusReq{
+				ReserveID: *it.ReserveID,
 			})
+			if err != nil {
+				log.Print(err)
+				outputErrorMsg(w, http.StatusInternalServerError, err.Error())
+				tx.Rollback()
+				return
+			}
+			itemDetails[i].ShippingStatus = ssr.Status
 		}
-	}
-
-	// すべての goroutine 完了を待つ
-	if err := g.Wait(); err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, err.Error())
-		tx.Rollback()
-		return
 	}
 
 	tx.Commit()
